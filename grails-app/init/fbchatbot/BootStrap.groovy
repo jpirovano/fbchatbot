ce1e5e8b8457e9dc3e2feb25f4d1b3c40f1b2144
Jonathan <jonathan.wilianpirovano@gmail.com>
2017-03-12 16:16:23 -0300

    first commit

---
+++ grails-app/init/fbchatbot/BootStrap.groovy
@@ -0,0 +1,9 @@
+package fbchatbot
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ grails-app/services/fbchatbot/FbchatbotService.groovy
@@ -0,0 +1,11 @@
+package fbchatbot
+
+import grails.transaction.Transactional
+
+@Transactional
+class FbchatbotService {
+
+    def serviceMethod() {
+
+    }
+}
